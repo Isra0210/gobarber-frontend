3a82daf6dab3ed309a11f0d7802f440f7d89eac9
Israel Rodrigues <r.israel0210@gmail.com>
2021-02-15 20:22:30 -0300

    signIn and signUp implemented

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import { Container, Content, Background } from './styles';
+import Input from '../../components/input';
+import Button from '../../components/button';
+import logoImg from '../../assets/logo.svg';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="Gobarber"/>
+
+      <form>
+        <h1>Faça seu Logon</h1>
+        
+        <Input name="email" icon={FiMail} placeholder="E-mail"/>
+        <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+
+        <Button type="submit">Entrar</Button>
+
+        <a href="forgot"> Esqueci minha senha</a>
+      </form>
+      <a href="http://">
+        <FiLogIn />
+        Criar conta 
+      </a>
+    </Content>
+    <Background/>
+  </Container>
+);
+
+export default SignIn;
